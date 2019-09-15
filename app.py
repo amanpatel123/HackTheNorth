8f192889112aae66ee6a2e26595e21ee143308fb
Nisarg Patel <patel1wq@uwindsor.ca>
2019-09-15 02:45:14 -0400

    Minor changes - Aman

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ PAGE_ACCESS_TOKEN = 'EAAGfED5iSXUBADpODELNQ5L91rQTVcc4oZBeNMPX6blm3qCpdIdnkcD5aG
 def get_bot_response(message):
     """This is just a dummy function, returning a variation of what
     the user said. Replace this function with one connected to chatbot."""
-    return "This is a dummy response to '{}'".format(message)
+    return "Okay, I will keep a track of the provided URL!".format(message)
 
 def send_message(recipient_id, text):
     """Send a response to Facebook"""
@@ -69,6 +69,15 @@ def is_user_message(message):
 def pushItem(url, userId):
     firebase = pyrebase.initialize_app(config)
     db = firebase.database()  
+    all_datas=db.child("DATA").get().val()
+    for key, data in all_datas.items():
+        user_id = data["userId"]
+        URL = data["URL"]
+        if user_id == userId and url == URL:
+            str = "Item is already being monitored. Kindly provide the URL of new element"
+            print("Item is already being monitored. Kindly provide the URL of new element")
+            send_message(userId, string)
+
     newItem = {"userId": userId, "pname": "null", "URL": url,   "initial_price": "null", "lowest_price":"null" ,"difference": "null" , "change": "null"}
     db.child("DATA").push(newItem)
 
@@ -82,13 +91,16 @@ def checkDatabaseTask(userId):
         if user_id==userId.val():
             change = data.child("change").value()
             if change==1:
+<<<<<<< Updated upstream
                 difference=data.child("difference").value()
                 string=str.format("The price goes down by %d", difference)
+=======
+                difference=data["difference"]
+                string=str.format("The price has decreased by %d, time to consider buying it!", difference)
+>>>>>>> Stashed changes
                 send_message(userId, string)
-                
+    
    
-
-
 @app.route("/webhook", methods=['GET','POST'])
 def listen():
     """This is the main function flask uses to 
@@ -104,6 +116,7 @@ def listen():
                 print("user sends msg")
                 text = x['message']['text']
                 sender_id = x['sender']['id']
+                checkInDataBase(text, sender_id)
                 pushItem(text, sender_id)
                 checkDatabaseTask(sender_id)
                 respond(sender_id, text)
@@ -112,7 +125,6 @@ def listen():
 @app.route("/")
 def home():
     return render_template("home.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
