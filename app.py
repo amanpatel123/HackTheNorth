c0034dacc19c41ff5b5a1f099f2221c4e047f532
Nisarg Patel <patel1wq@uwindsor.ca>
2019-09-15 07:34:27 -0400

    jadhi

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ PAGE_ACCESS_TOKEN = 'EAAGfED5iSXUBADpODELNQ5L91rQTVcc4oZBeNMPX6blm3qCpdIdnkcD5aG
 def get_bot_response(message):
     """This is just a dummy function, returning a variation of what
     the user said. Replace this function with one connected to chatbot."""
-    return "Okay, I will keep a track of the provided URL!".format(message)
+    return ('Okay, I will keep a track of the provided URL!'.format(message))
 
 def send_message(recipient_id, text):
     """Send a response to Facebook"""
@@ -96,12 +96,12 @@ def checkDatabaseTask(userId):
     db = firebase.database()
     
     all_datas=db.child("DATA").get().val()
-    for key, data in all_datas:
+    for key, data in all_datas.items():
         user_id=data.child("userId").get()
-        if user_id==userId.val():
-            change = data.child("change").value()
+        if user_id==data["userId"]:
+            change = data.["change"]
             if change==1:
-                difference=data.child("difference").value()
+                difference=data["difference"]
                 string=str.format("The price goes down by %d", difference)
                 difference=data["difference"]
                 string=str.format("The price has decreased by %d, time to consider buying it!", difference)
